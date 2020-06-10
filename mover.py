e22e6032172f67802f1070277f5e580d70661347
Matt Argyle <matt.coding@gmail.com>
2020-06-10 13:40:15 -0400

    Update mover.py

--- mover.py
+++ mover.py
@@ -2,12 +2,9 @@ import time, requests, json
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-headers = {'content-type': 'application/json',
-'X-FilesAPI-Key': 'YOUR FILES.COM API KEY'
-}
-
 class Watcher:
     DIRECTORY_TO_WATCH = "img"
+
     def __init__(self):
         self.observer = Observer()
 
@@ -26,47 +23,52 @@ class Watcher:
 
 
 class Handler(FileSystemEventHandler):
+
     @staticmethod
     def on_any_event(event):
         if event.is_directory:
             return None
 
         elif event.event_type == 'created':
-
             print("Received created event - %s." % event.src_path)
 
         elif event.event_type == 'modified':
             print ("Received modified event - %s." % event.src_path)
 
-            
+            filePath = event.src_path
             strippedFile = event.src_path.replace('img/','')
+            #call the files.com API upload function
+            files_api_upload(strippedFile,filePath)
             
-            #make start upload call to files.com API(file upload is a three step process)
-            urlBase1 = 'https://YOURSUBDIRECTORY.files.com/api/rest/v1/files/YOUR_DIRECTORY/' + strippedFile + '/?action=put'
-            #debug
-            print ('****url base=' + urlBase1)
 
-            r = requests.post(urlBase1, headers=headers)
+def files_api_upload(strippedFile, filePath):
+            headers = {'content-type': 'application/json',
+            'X-FilesAPI-Key': '**YOUR API KEY***'
+            }
+            urlBase = 'https://**YOUR_SUBDOMAIN**.files.com/api/rest/v1/files/catBot/'
+
+            #make start upload call to files.com API
+            startUploadUrl = urlBase + strippedFile + '/?action=put'
+
+            r = requests.post(startUploadUrl, headers=headers)
             jsonResponse = r.json()
             print(jsonResponse)
-            #get vars for subsequent calls
+
             uploadUri = jsonResponse['upload_uri']
             uploadRef = jsonResponse['ref']
 
             #upload actual file
-            data = open(event.src_path, 'rb').read()
+            data = open(filePath, 'rb').read()
             r2 = requests.put(url=uploadUri,
                     data=data,
                     headers={'Content-Type': 'application/octet-stream'})
             print(r2)
 
             #close out the upload!
-            urlBase2 = 'https://YOURSUBDIRECTORY.files.com/api/rest/v1/files/YOUR_DIRECTORY/' + strippedFile + '/?action=end&ref=' + uploadRef
+            endUploadUri = urlBase + strippedFile + '/?action=end&ref=' + uploadRef
             #send the request
-            r3 = requests.post(urlBase2, headers=headers)
+            r3 = requests.post(endUploadUir, headers=headers)
             print(r3)
-
-
 
 if __name__ == '__main__':
     w = Watcher()
